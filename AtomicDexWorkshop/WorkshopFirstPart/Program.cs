3480fe23e4eb13b2fd06c3f542b9fdfadb93c5ec
romanszterg <rmscastle@gmail.com>
2020-01-22 20:58:52 +0100

    feat(init): init repo

---
+++ AtomicDexWorkshop/WorkshopFirstPart/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkshopFirstPart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
